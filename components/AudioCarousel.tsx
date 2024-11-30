f7797391be13198dac861396267a27c7178ab88b
Ian Hudicourt <ihudicourt@gmail.com>
2024-11-30 14:22:58 -0500

    added library components

---
+++ components/AudioCarousel.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import { Audio } from "@prisma/client";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import AudioCard from "./AudioCard";
+
+interface AudioCarouselProps {
+    tracks: Audio[];
+    onSelect: (track: Audio) => void;
+}
+
+const AudioCarousel: React.FC<AudioCarouselProps> = ({ tracks, onSelect }) => {
+    return (
+        <Swiper
+            modules={[Navigation, Pagination]}
+            spaceBetween={1}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            breakpoints={{
+                640: {
+                    slidesPerView: 2.5,
+                },
+                768: {
+                    slidesPerView: 3,
+                },
+                1024: {
+                    slidesPerView: 6,
+                },
+                1280: {
+                    slidesPerView: 8,
+                },
+            }}
+        >
+            {tracks.map((track) => (
+                <SwiperSlide key={track.id}>
+                    <AudioCard track={track} onSelect={onSelect} />
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    );
+};
+
+export default AudioCarousel;
