958ab844ddc28190ede08424d5b75da5bba4b8cd
Ian Hudicourt <ihudicourt@gmail.com>
2024-11-30 14:20:27 -0500

    added track routes

---
+++ app/api/tracks/[id]/route.ts
@@ -0,0 +1,119 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/../utils/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/route";
+
+export async function POST(request: NextRequest) {
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user?.email) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const { title, artist, album, duration, s3Key } = await request.json();
+
+    if (!title || !artist || !duration || !s3Key) {
+        return NextResponse.json(
+            { error: "Missing required fields" },
+            { status: 400 }
+        );
+    }
+
+    try {
+        // Find the user in the database
+        const user = await prisma.user.findUnique({
+            where: { email: session.user.email },
+        });
+
+        if (!user) {
+            return NextResponse.json(
+                { error: "User not found" },
+                { status: 404 }
+            );
+        }
+
+        const url = new URL(request.url);
+        const id = url.searchParams.get("id");
+
+        if (!id) {
+            return NextResponse.json(
+                { error: "Missing audioId parameter" },
+                { status: 400 }
+            );
+        }
+
+        // Save the audio metadata and file URL to the database
+        const track = await prisma.audio.update({
+            data: {
+                title,
+                artist,
+                album,
+                duration: parseInt(duration), // Duration in seconds
+                s3Key,
+                userId: user.id,
+            },
+            where: { id },
+        });
+
+        return NextResponse.json(
+            {
+                message: "Track updated successfully",
+                track,
+            },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.error("Error saving track:", error);
+        return NextResponse.json(
+            { error: "Error saving track" },
+            { status: 500 }
+        );
+    }
+}
+
+export async function GET(request: NextRequest) {
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user?.email) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    try {
+        // Find the user in the database
+        const user = await prisma.user.findUnique({
+            where: { email: session.user.email },
+        });
+
+        if (!user) {
+            return NextResponse.json(
+                { error: "User not found" },
+                { status: 404 }
+            );
+        }
+
+        const url = new URL(request.url);
+        const id = url.searchParams.get("id");
+
+        if (!id) {
+            return NextResponse.json(
+                { error: "Missing audioId parameter" },
+                { status: 400 }
+            );
+        }
+
+        // Get all tracks for the authenticated user
+        const track = await prisma.audio.findUniqueOrThrow({
+            where: {
+                id,
+            },
+        });
+
+        return NextResponse.json({ track }, { status: 200 });
+    } catch (error) {
+        console.error("Error fetching track:", error);
+        return NextResponse.json(
+            { error: "Error fetching track" },
+            { status: 500 }
+        );
+    }
+}
---
+++ app/api/tracks/upload-url/route.ts
@@ -0,0 +1,52 @@
+import { NextRequest, NextResponse } from "next/server";
+import AWS from "aws-sdk";
+import { nanoid } from "nanoid";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/api/auth/[...nextauth]/route";
+
+AWS.config.update({
+    region: process.env.AWS_REGION!,
+    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.AWS_SECRET_KEY!,
+});
+
+const s3 = new AWS.S3();
+
+export async function POST(request: NextRequest) {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const { name, type } = await request.json();
+
+    if (!name || !type) {
+        return NextResponse.json(
+            { error: "Missing required parameters" },
+            { status: 400 }
+        );
+    }
+
+    const fileExtension = name.substring(name.lastIndexOf("."));
+    const fileName = `${nanoid()}${fileExtension}`;
+
+    const s3Params = {
+        Bucket: process.env.AWS_BUCKET_NAME!,
+        Key: fileName,
+        Expires: 600,
+        ContentType: type,
+    };
+
+    try {
+        const uploadURL = await s3.getSignedUrlPromise("putObject", s3Params);
+
+        return NextResponse.json({ uploadURL, key: fileName });
+    } catch (error) {
+        console.error("Error generating pre-signed URL", error);
+        return NextResponse.json(
+            { error: "Error generating pre-signed URL" },
+            { status: 500 }
+        );
+    }
+}
